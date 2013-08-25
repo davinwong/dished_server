a5574cf5df5e1d86b346f2edf2dc565d69a2eabe
davinwong <davinwong@outlook.com>
2013-08-25 15:24:24 -0400

    commented heroku settings

--- dished_server/settings.py
+++ dished_server/settings.py
@@ -159,10 +159,10 @@ LOGGING = {
     }
 }
 
-# HEROKU
-# Parse database configuration from $DATABASE_URL
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+### HEROKU - Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+### HEROKU
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
