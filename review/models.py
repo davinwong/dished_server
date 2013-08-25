c1578ebc2e7f3f79f89c3fca285589837f44f857
davinwong <davinwong@outlook.com>
2013-08-25 17:06:37 -0400

    start review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from dish.models import Dish
 
-# Create your models here.
+class Review(models.Model):
+    """
+    speed measured in minutes
+    service? should be part of restaurant, but we review dishes
+    """
+    dish = models.ForeignKey(Dish)
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    size = models.DecimalField(max_digits=4, decimal_places=3, null=True, blank=True)
+    overall = models.DecimalField(max_digits=4, decimal_places=3)
+    spice = models.DecimalField(max_digits=4, decimal_places=3)
+    sweet = models.DecimalField(max_digits=4, decimal_places=3)
+    salt = models.DecimalField(max_digits=4, decimal_places=3)
+    presentation = models.DecimalField(max_digits=4, decimal_places=3)
+    speed = models.IntegerField()
