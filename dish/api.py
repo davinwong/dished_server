d766d1c0c4efec091d077e40400631adb5610c26
davinwong <davinwong@outlook.com>
2013-08-25 23:30:52 -0400

    api start allowed_methods, authenticate, authorize

--- dish/api.py
+++ dish/api.py
@@ -1,17 +1,30 @@
 from tastypie.resources import ModelResource
 from dish.models import Restaurant, Dish, Review
 
+# TODO: better authentication
 
 class RestaurantResource(ModelResource):
     class Meta:
         queryset = Restaurant.objects.all()
+        allowed_methods = ['get', 'post']
+        authentication = BasicAuthentication()
+        authorization = Authorization()
         # resource_name = 'restaurant'
+
 
 class DishResource(ModelResource):
 	class Meta:
 		queryset = Dish.objects.all()
+		allowed_methods = ['get', 'post']
+		authentication = BasicAuthentication()
+		authorization = Authorization()
 		# resource_name = 'dish'
 
+
+# TODO: authorization: users can only edit their own reviews
 class ReviewResource(ModelResource):
 	class Meta:
 		queryset = Review.objects.all()
+		allowed_methods = ['get', 'delete', 'post', 'put']
+		authentication = BasicAuthentication()
+		authorization = Authorization()
